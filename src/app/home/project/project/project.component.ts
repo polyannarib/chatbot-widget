6286c49eac95423fd658e7fd7f0d8a9b29ca8025
Lorena Costa <lorenacosta.ufu@gmail.com>
2019-06-17 13:40:05 -0300

    Inicio tela projeto

--- src/app/home/project/project/project.component.ts
+++ src/app/home/project/project/project.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
+  projects: any[];
+
   constructor() { }
 
   ngOnInit() {
+    this.projects = [
+      {
+        id: 9098080808,
+        name: "Workplayer",
+        description: "Sistema de gamefication da Kyros",
+
+      },
+      {
+        id: 9098080808,
+        name: "Outro Projeto",
+        description: "Sistema qualquer",
+
+      },
+      {
+        id: 909234808,
+        name: "Ótimo Projeto",
+        description: "Um ótimo projeto",
+
+      }
+    ]
   }
 
 }
