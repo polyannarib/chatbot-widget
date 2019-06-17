64250620b1e6b0f6b491041c29489d6202176d8e
Tiago Oliveira Santos <tgosts@algartelcom.com.br>
2019-06-17 09:12:00 -0300

    Ajuste login + bradesco.color.red

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,57 +12,57 @@ export class AuthService {
   private usuarioAutenticado: boolean = false; /// remover em breve
   private user: User;
   private url: string = environment.back_end_url;
-  private header:any = {'Content-Type':'application/json'};
+  private header: any = { 'Content-Type': 'application/json' };
 
-  constructor(private router: Router, 
-              private http: HttpClient ) { }
+  constructor(private router: Router,
+    private http: HttpClient) { }
 
-  login(user: User){
-
-    if(user.username.includes('kyros.com.br')){
-      var body = {
-        email : user.username ,
-        password : btoa(user.password)
-      }
-
-      this.http.post(this.url + '/login',body,
-      {observe:'response',headers:new HttpHeaders(this.header)}).subscribe(
-        resp => { 
-
-          
-                  if( resp.body != undefined && resp.body['status'] == 0 ){
-                    this.user = new User();
-                    this.user.token = resp.headers.get('X-Token');
-                    let userInfo = resp.body['object'];
-                    this.user.username = userInfo['username'];
-                    this.user.companyName = userInfo['companyName'];
-                    this.user.displayName = userInfo['displayName'];
-                    this.user.email = userInfo['email'];
-                    this.user.permissions = userInfo['permissions'];
-                    this.user.scopes = userInfo['scopes'];
+  login(user: User) {
 
 
-                    this.usuarioAutenticado = true;
-
-                    // TODO: retirar isso, o ideal é salvar o token
-                    localStorage.setItem('user',JSON.stringify(this.user));
-                    this.router.navigate(['/home/dashboard']);
-
-                  }else{
-                    alert(resp.body["message"]);
-                  }
-      });
-
+    var body = {
+      email: user.username,
+      password: btoa(user.password)
     }
+
+    this.http.post(this.url + '/login', body,
+      { observe: 'response', headers: new HttpHeaders(this.header) }).subscribe(
+        resp => {
+
+
+          if (resp.body != undefined && resp.body['status'] == 0) {
+            this.user = new User();
+            this.user.token = resp.headers.get('X-Token');
+            let userInfo = resp.body['object'];
+            this.user.username = userInfo['username'];
+            this.user.companyName = userInfo['companyName'];
+            this.user.displayName = userInfo['displayName'];
+            this.user.email = userInfo['email'];
+            this.user.permissions = userInfo['permissions'];
+            this.user.scopes = userInfo['scopes'];
+
+
+            this.usuarioAutenticado = true;
+
+            // TODO: retirar isso, o ideal é salvar o token
+            localStorage.setItem('user', JSON.stringify(this.user));
+            this.router.navigate(['/home/dashboard']);
+
+          } else {
+            alert(resp.body["message"]);
+          }
+        });
+
+
   }
 
-  usuarioEstaAutenticado(){
+  usuarioEstaAutenticado() {
     return this.usuarioAutenticado;
   }
 
-  getUser(){
-    if(!this.user) {
-      let user:User = JSON.parse(localStorage.getItem('user'));
+  getUser() {
+    if (!this.user) {
+      let user: User = JSON.parse(localStorage.getItem('user'));
       return user;
     }
     return this.user;
