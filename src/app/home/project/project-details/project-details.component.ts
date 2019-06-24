54e98c9fa84a28dcdbbad87d46dfac301de04186
Lorena Costa <lorenacosta.ufu@gmail.com>
2019-06-24 15:49:51 -0300

    Recuperando id

--- src/app/home/project/project-details/project-details.component.ts
+++ src/app/home/project/project-details/project-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project-details',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = +params['id'];
+   });
   }
 
 }
