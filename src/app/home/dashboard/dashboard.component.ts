c57a1aca7d3f53779fb6c7278004f1fbbb02731d
Lorena Costa <lorenacosta.ufu@gmail.com>
2019-06-26 13:37:07 -0300

    Graficos mais dinamicos

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit, ElementRef, EventEmitter } from '@angular/core';
-import { ChartOptions, ChartType } from 'chart.js';
-import { Label, SingleDataSet} from 'ng2-charts';
-import { MaterializeAction } from 'angular2-materialize';
-//import $ from '';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { ChartOptions } from 'chart.js';
 
 declare var jQuery: any;
 
@@ -12,87 +9,43 @@ declare var jQuery: any;
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  title = 'Gamefication';
 
-  public chartColors: Array<any> = [
-    { // first color
-      backgroundColor:['rgba(251, 200, 68, 0.8)', 'rgba(150, 150, 150, 0.8)'] ,
-      pointBackgroundColor: ['rgba(225,10,24,0.2)','purple'],
-    }];
-  public pieChartType: ChartType = 'pie';
-  public pieChartLegend = false;
-  public pieChartOptions: ChartOptions = {
-    responsive: true,
-  };
   private graphics: Array<any> = [];
-  //public pieChartTitle: Title = 'teste';
-  public pieChartLabels: Label[] = ['Recurso Disp', 'Recurso Prog'];
-  public pieChartData: SingleDataSet = [1500, 800];
-
-
-  public pieChartLabels2: Label[] = ['Rec. no prazo', 'Rec. atrasados'];
-  public pieChartData2: SingleDataSet = [98, 2];
-  public pieChartLegend2 = false;
-
-  public pieChartLabels3: Label[] = ['Req. no prazo', 'Req. atrasados'];
-  public pieChartData3: SingleDataSet = [15, 7];
-  public pieChartLegend3 = false;
-
-  public pieChartLabels4: Label[] = ['Pjt. no prazo', 'Pjt. atrasados'];
-  public pieChartData4: SingleDataSet = [5, 2];
-  public pieChartLegend4 = false;
-
-  public pieChartLabels5: Label[] = ['Req. no prazo', 'Req. atrasados'];
-  public pieChartData5: SingleDataSet = [3, 2];
-  public pieChartLegend5 = false;
-
-  public pieChartLabels6: Label[] = ['Projeto sem alteração', 'Projeto alterado'];
-  public pieChartData6: SingleDataSet = [3, 4];
-  public pieChartLegend6 = false;
-
-  public pieChartLabels7: Label[] = ['Conhecimento utilizado', 'Conhecimento não utilizado'];
-  public pieChartData7: SingleDataSet = [70, 30];
-  public pieChartLegend7 = false;
-
-  modalActions = new EventEmitter<string|MaterializeAction>();
-
+  private chartColors: Array<any> = [];
+  private chartOptions: ChartOptions;
+ 
   constructor( private el: ElementRef) {}
 
   ngOnInit() {
     jQuery(this.el.nativeElement).find('.collapsible').collapsible();
     var main = getComputedStyle(document.body).getPropertyValue('--graph-main-color');
     var second = getComputedStyle(document.body).getPropertyValue('--graph-color');
-    this.chartColors[0].backgroundColor = [main,second];
+    this.chartColors.push({
+      backgroundColor: [main,second],
+      pointBackgroundColor: [main,second]
+    })
+    this.chartOptions = {
+      responsive: true,
+    };
+    
+    // Para teste
     this.graphics.push({
-      title: "Gráfico",
+      title: "Projeto no prazo",
       data: [5,2],
-      labels: ['Conhecimento utilizado', 'Conhecimento não utilizado'],
+      labels: ['Projeto no prazo', 'Projeto atrasado'],
       type: 'pie'
     })
     this.graphics.push({
-      title: "Gráfico2",
+      title: "Recurso utilizado",
       data: [5,20],
       labels: ['Recurso utilizado', 'Recurso não utilizado'],
       type: 'pie'
     })
     this.graphics.push({
-      title: "Gráfico3",
+      title: "Cards",
       data: [15,2,8],
       labels: ['JS', 'Java','.Net'],
       type: 'bar'
     })
   }
-
-  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
-
-  openModal() {
-    this.modalActions.emit({action:"modal",params:['open']});
-  }
-
-  // getChartType(type: string): ChartType{
-    
-  // }
-
 }
