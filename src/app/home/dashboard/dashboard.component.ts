03ff948d4cc4f225bdd418ae2803ed0a7db7d8f4
Julian Degutis de Freitas Garcia <juliang@kyros.com.br>
2019-09-26 11:14:38 -0300

    Remoção do Chart

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -95,55 +95,6 @@ export class DashboardComponent implements OnInit {
     this.endDate.setDate(this.startDate.getDate() + 13);
     this.endDateProject.setDate(this.startDate.getDate() + 7);
 
-    jQuery(this.el.nativeElement).find('.collapsible').collapsible();
-    var main = getComputedStyle(document.body).getPropertyValue('--graph-main-color');
-    var second = getComputedStyle(document.body).getPropertyValue('--graph-color');
-    this.chartColors.push({
-      backgroundColor: [main, second],
-      pointBackgroundColor: [main, second]
-    })
-    this.chartOptions = {
-      responsive: true,
-    };
-
-    // Para teste
-    this.graphics.push({
-      title: "Projeto no prazo",
-      data: [5, 2],
-      labels: ['Projeto no prazo', 'Projeto atrasado'],
-      type: 'pie'
-    })
-    this.graphics.push({
-      title: "Recurso utilizado",
-      data: [5, 20],
-      labels: ['Recurso utilizado', 'Recurso não utilizado'],
-      type: 'pie'
-    })
-    this.graphics.push({
-      title: "Cards Utilizados",
-      data: [15, 2],
-      labels: ['JS', 'Java'],
-      type: 'pie'
-    })
-    this.graphics.push({
-      title: "Projeto alterado",
-      data: [15, 5],
-      labels: ['Projetos alterados', 'Projeto não alterados'],
-      type: 'pie'
-    })
-    this.graphics.push({
-      title: "Recurso disponível",
-      data: [15, 2],
-      labels: ['Recurso disponível', 'Recurso não disponível'],
-      type: 'pie'
-    })
-    this.graphics.push({
-      title: "Projetos no prazo",
-      data: [15, 2],
-      labels: ['Projetos no prazo', 'Projetos atrasados'],
-      type: 'pie'
-    })
-
     Promise.all([
       new Promise(function (resolve, reject) {
         let params = {
