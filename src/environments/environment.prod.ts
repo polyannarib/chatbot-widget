d7fddf980884ebd28da1ceac0fe23e465b4a7a4f
Gustavo Henrique Menezes de Paulo <gustavop@kyros.com.br>
2019-11-12 17:04:57 -0300

    Subindo atualização do environment apontado para https://www.kyros.com.br e correção do bug de finalizar a taks sem uma data de inicio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  back_end_url: 'http://192.168.1.229:8593/workplayer-portal/services',
+  back_end_url: 'https://www.kyros.com.br/workplayer-portal/services',
   production: false
 };
 
