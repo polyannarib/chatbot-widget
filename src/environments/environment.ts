4553f83b3ff4146a90144075316d45d0f873c475
Gustavo Henrique Menezes de Paulo <gustavop@kyros.com.br>
2019-11-13 14:19:27 -0300

    criado o  modal do status list report dentro do componente dashboard

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   production: false
 };
 
-export var company:string = 'bradesco';
+export var company: string = 'bradesco';
 
 /*
  * For easier debugging in development mode, you can import the following file
