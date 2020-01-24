171f4e1d5e9ed6b90c0ee2f8ff72ce8b52008faf
Gustavo Henrique Menezes de Paulo <gustavop@kyros.com.br>
2020-01-24 15:30:52 -0300

    subindo atualizacao

--- src/app/core/services/card.service.ts
+++ src/app/core/services/card.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { isEmpty } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -32,6 +33,13 @@ export class CardService {
   
   giveToPlayer(data): Observable<any> {
     return this.http.post(`${environment.back_end_url}/${this.prefixService}/giveToPlayer`, data);
+  }
+
+  KnowledgeIn(data?: any): Observable<any> {
+    if(!data) {
+      return this.http.get(`${environment.back_end_url}/${this.prefixService}/knowledgeIn`);
+    }
+    return this.http.get(`${environment.back_end_url}/${this.prefixService}/knowledgeIn?knowledgeParentIds=${data}`);
   }
 
 }
--- src/app/modules/management/resource/resource-find/resource-find.component.ts
+++ src/app/modules/management/resource/resource-find/resource-find.component.ts
@@ -1,44 +1,33 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { Observable, ReplaySubject } from 'rxjs';
-import { startWith, map } from 'rxjs/operators';
-import { MatSelect } from '@angular/material/select';
-
-/* import { SelectAutocompleteComponent } from 'mat-select-autocomplete'; */
+import { SelectAutocompleteComponent } from 'mat-select-autocomplete';
+import { CardService } from 'src/app/core/services/card.service';
 
 @Component({
   selector: 'app-resource-find',
   templateUrl: './resource-find.component.html',
-  styleUrls: ['./resource-find.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./resource-find.component.css']
 })
 export class ResourceFindComponent implements OnInit {
 
-  toppings = new FormControl();
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-
-  // @ViewChild('singleSelect', { static: true }) singleSelect: MatSelect;
+  aux: number = 1;
+  parentIds: number[] = [];
   
-  // /** list of banks */
-  // protected banks: any = [
-  //   { id: 1, name: "Bradesco" },
-  //   { id: 2, name: "Santander" },
-  //   { id: 3, name: "Itau" },
-  //   { id: 4, name: "Nubank" },
-  //   { id: 5, name: "Inter" },
-  //   { id: 6, name: "Caixa" },
-  //   { id: 7, name: "Banco do Brasil" }
-  // ];
+  constructor(
+    private cardService: CardService
+  ) { }
 
-  // /** control for the selected bank */
-  // public bankCtrl: FormControl = new FormControl();
-  
-  // /** control for the MatSelect filter keyword */
-  // public bankFilterCtrl: FormControl = new FormControl();
-  // public filteredBanks: ReplaySubject<any> = new ReplaySubject<any>(1);
-  
-  constructor() { }
+  ngOnInit() {
+    this.getKnowledgeIn();
+  }
 
-  ngOnInit() { }
+  getKnowledgeIn() {
+    this.cardService.KnowledgeIn().subscribe(
+      (response) => {
+        
+      },(error) => {
+        
+    })
+  }
 
 }
