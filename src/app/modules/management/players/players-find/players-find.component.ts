392b3d66010e08142a052fa4330272328dd778e5
Gustavo Henrique Menezes de Paulo <ghmenezes@live.com>
2020-06-18 22:41:57 -0300

    atualizando problemas

--- src/app/modules/management/players/players-find/players-find.component.ts
+++ src/app/modules/management/players/players-find/players-find.component.ts
@@ -141,7 +141,7 @@ export class PlayersFindComponent implements OnInit {
       const cards = this.cards.map(element => element.root);
       this.playerRatedFilter = this.playerRated.filter((element) => {
         const card = cards.map(cardsName => {
-          if(element.cards[cardsName] && (element.cards[cardsName] == 1 || element.cards[cardsName] == 0)) {
+          if(element.cards[cardsName] == 1 || element.cards[cardsName] == 0) {
             return true;
           } 
           return false;
