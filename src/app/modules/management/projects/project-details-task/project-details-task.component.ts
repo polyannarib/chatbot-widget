8711a9b646d68e45806147b8959f3b55521fece8
antonion <antonion@kyros.com.br>
2020-08-26 15:48:58 -0300

    remoção da filtragem feita pelo front

--- src/app/modules/management/projects/project-details-task/project-details-task.component.ts
+++ src/app/modules/management/projects/project-details-task/project-details-task.component.ts
@@ -46,8 +46,7 @@ export class ProjectDetailsTaskComponent implements OnInit {
     this.taskService.getTasksByProject(id).subscribe(
       (response) => {
         if(response.status == 0) {          
-          console.log(response);
-          this.tasks = response.object.filter(element => element.rule == null || element.rule.name == 'ATIVIDADE');
+          this.tasks = response.object;
           this.dataSource.data = this.tasks;
           this.loader = false;
           return;
